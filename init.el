882f5e1e67c6de5014848d469934f4e8e2a14598
Stephan Thiel <stephan@nand.io>
2016-05-10 18:40:25 +0200

    load material-light by default add auto-generated changes to init.el

--- init.el
+++ init.el
@@ -1,15 +1,56 @@
 ;; -*- emacs-lisp -*-
 
 ;; turn off splash screen messages
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (setq inhibit-startup-echo-area-message t
       inhibit-startup-screen t)
 
 ;; no window chrome!
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(column-number-mode t)
+ '(custom-safe-themes
+   (quote
+    ("67ca766b07ce92be5c0a0111930378d724e9927c890675d4847a774daabc8eaa" "53fc4871f519e34fd25604a5391272527b2dbb4e365b97615615e5bb8c9f16b6" default)))
+ '(fci-rule-color "#ECEFF1")
+ '(hl-sexp-background-color "#efebe9")
  '(menu-bar-mode nil)
+ '(package-selected-packages
+   (quote
+    (yaml-mode visual-regexp undo-tree tuareg smex smartparens slime-company skewer-mode robe restclient rainbow-mode processing-mode pretty-symbols powerline parenface-plus paredit pallet motion-mode markdown-mode magit json-mode js2-refactor ido-ubiquitous highlight haml-mode geiser fringe-helper flymake-ruby flymake-coffee find-file-in-project expand-region elisp-slime-nav diminish company-inf-ruby company-go company-ghc color-theme coffee-mode cider bundler aggressive-indent ag ack-and-a-half ace-jump-mode)))
  '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(vc-annotate-background nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#B71C1C")
+     (40 . "#FF5722")
+     (60 . "#FFA000")
+     (80 . "#558b2f")
+     (100 . "#00796b")
+     (120 . "#2196f3")
+     (140 . "#4527A0")
+     (160 . "#B71C1C")
+     (180 . "#FF5722")
+     (200 . "#FFA000")
+     (220 . "#558b2f")
+     (240 . "#00796b")
+     (260 . "#2196f3")
+     (280 . "#4527A0")
+     (300 . "#B71C1C")
+     (320 . "#FF5722")
+     (340 . "#FFA000")
+     (360 . "#558b2f"))))
+ '(vc-annotate-very-old-color nil))
 
 ;; I have loads of RAM, don't start GC until I've used a decent chunk
 ;; of it.
@@ -56,3 +97,16 @@
 ;;(define-key global-map "<s-E>" 'org-capture)
 
 ;; TODO bring in more web development stuff
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-scrollbar-bg ((t (:background "#2f2f2f"))))
+ '(company-scrollbar-fg ((t (:background "#222222"))))
+ '(company-tooltip ((t (:inherit default :background "#1b1b1b"))))
+ '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
+ '(company-tooltip-common-selection ((t (:inherit font-lock-keyword-face))))
+ '(company-tooltip-selection ((t (:inherit font-lock-keyword-face)))))
+
+(load-theme 'material-light t)
